511d7676ba3f94f27903e33b874caeeee564902c
AndyMDV <jun19981708@hotmail.com>
2023-11-15 00:18:39 -0500

    updates

--- src/provider/BillboardProvaider.tsx
+++ src/provider/BillboardProvaider.tsx
@@ -6,3 +6,63 @@ import {
     useState,
 } from "react";
 import { useBillboard } from "../hooks";
+import { BillboardRequest } from "../domain/butacaRequest";
+
+export interface IBillboardContext {
+    //Parametros de accion del modal
+    isOpenModal: boolean;
+    setIsCloseModal: Dispatch<SetStateAction<boolean>>;
+
+    isEditModal: boolean;
+    setIsEditModal: Dispatch<SetStateAction<boolean>>;
+
+    setDateBillboard: Dispatch<SetStateAction<string>>;
+    setNameMovie: Dispatch<SetStateAction<string>>;
+    setRoomNumber: Dispatch<SetStateAction<string>>;
+
+    billboardList: BillboardRequest[];
+
+    runDeleteBillboard: () => Promise<void>;
+    runEditBillboard: () => Promise<void>;
+    runGetBillboardList: () => Promise<void>;
+    runSaveBillboard: () => Promise<void>;
+}
+
+const BillboardContext = createContext({});
+
+export const BillboardProvaider = ({ children }: { children: ReactNode }) => {
+    const [isOpenModal, setIsCloseModal] = useState<boolean>(false);
+    const [isEditModal, setIsEditModal] = useState<boolean>(false);
+
+    const {
+        billboardList,
+        runDeleteBillboard,
+        runEditBillboard,
+        runGetBillboardList,
+        runSaveBillboard,
+        setDateBillboard,
+        setNameMovie,
+        setRoomNumber
+    } = useBillboard();
+
+    const storage: IBillboardContext = {
+        isOpenModal,
+        setIsCloseModal,
+        isEditModal,
+        setIsEditModal,
+        setDateBillboard,
+        setNameMovie,
+        setRoomNumber,
+        billboardList,
+        runDeleteBillboard,
+        runEditBillboard,
+        runGetBillboardList,
+        runSaveBillboard,
+    }
+
+    return (
+        <BillboardContext.Provider value={storage}>{children}</BillboardContext.Provider>
+    )
+}
+
+export default BillboardContext;
--- src/provider/BookingProvaider.tsx
+++ src/provider/BookingProvaider.tsx
@@ -4,5 +4,72 @@ import {
     Dispatch,
     SetStateAction,
     useState,
-  } from "react";
+} from "react";
 import { useBooking } from "../hooks";
+import { BookingRequest } from "../domain/butacaRequest";
+
+export interface IBookingContext {
+    //Parametros de accion del modal
+    isOpenModal: boolean;
+    setIsCloseModal: Dispatch<SetStateAction<boolean>>;
+
+    isEditModal: boolean;
+    setIsEditModal: Dispatch<SetStateAction<boolean>>;
+
+    bookingList: BookingRequest[] | undefined;
+
+    setBooking: Dispatch<SetStateAction<BookingRequest | undefined>>;
+    setDateBillboard: Dispatch<SetStateAction<string>>;
+    setDateBook: Dispatch<SetStateAction<string>>;
+    setDocument: Dispatch<SetStateAction<string>>;
+    setSeatNumber: Dispatch<SetStateAction<string>>;
+
+    runDeleteBooking: () => Promise<void>;
+    runEditBooking: () => Promise<void>;
+    runGetBookingList: () => Promise<void>;
+    runSaveBookking: () => Promise<void>;
+}
+
+const BookingContext = createContext({});
+
+export const BookingProvaider = ({ children }: { children: ReactNode }) => {
+
+    const [isOpenModal, setIsCloseModal] = useState<boolean>(false);
+    const [isEditModal, setIsEditModal] = useState<boolean>(false);
+
+    const {
+        bookingList,
+        runDeleteBooking,
+        runEditBooking,
+        runGetBookingList,
+        runSaveBookking,
+        setBooking,
+        setDateBillboard,
+        setDateBook,
+        setDocument,
+        setSeatNumber
+    } = useBooking();
+
+    const storage: IBookingContext = {
+        isOpenModal,
+        setIsCloseModal,
+        isEditModal,
+        setIsEditModal,
+        setBooking,
+        setDateBillboard,
+        setDateBook,
+        setDocument,
+        setSeatNumber,
+        bookingList,
+        runDeleteBooking,
+        runEditBooking,
+        runGetBookingList,
+        runSaveBookking,
+    }
+
+    return (
+        <BookingContext.Provider value={storage}>{children}</BookingContext.Provider>
+    );
+}
+
+export default BookingContext;
